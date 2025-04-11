f73cb1387d83097f606de84705d9a0d507da055b
KunnuSherry <gamingvenom465@gmail.com>
2025-04-11 14:16:00 +0530

    c

--- static/components/homeAnimation.js
+++ static/components/homeAnimation.js
@@ -1,211 +1,276 @@
-import gsap from "https://cdn.skypack.dev/gsap"
-import { ScrollTrigger } from "https://cdn.skypack.dev/gsap/ScrollTrigger"
-import { MotionPathPlugin } from "https://cdn.skypack.dev/gsap/MotionPathPlugin"
+import imagesLoaded from "https://esm.sh/imagesloaded";
 
-gsap.registerPlugin(ScrollTrigger)
-gsap.registerPlugin(MotionPathPlugin)
+console.clear();
 
-const rx = window.innerWidth < 1000 ? window.innerWidth / 1200 : 1
-const ry = window.innerHeight < 700 ? window.innerHeight / 1200 : 1
+// -------------------------------------------------
+// ------------------ Utilities --------------------
+// -------------------------------------------------
 
-const path = [
-			// 1
-			{ x: 800, y: 200 },
-			{ x: 900, y: 20 },
-			{ x: 1100, y: 100 },
-			// 2
-			{ x: 1000, y: 200 },
-			{ x: 900, y: 20 },
-			{ x: 10, y: 500 },
-			// 3
-			{ x: 100, y: 300 },
-			{ x: 500, y: 400 },
-			{ x: 1000, y: 200 },
-			// 4
-			{ x: 1100, y: 300 },
-			{ x: 400, y: 400 },
-			{ x: 200, y: 250 },
-			// 5
-			{ x: 100, y: 300 },
-			{ x: 500, y: 450 },
-			{ x: 1100, y: 500 }
-		]
+// Math utilities
+const wrap = (n, max) => (n + max) % max;
+const lerp = (a, b, t) => a + (b - a) * t;
 
-const scaledPath = path.map(({ x, y }) => {
+// DOM utilities
+const isHTMLElement = (el) => el instanceof HTMLElement;
+
+const genId = (() => {
+	let count = 0;
+	return () => {
+		return (count++).toString();
+	};
+})();
+
+class Raf {
+	constructor() {
+		this.rafId = 0;
+		this.raf = this.raf.bind(this);
+		this.callbacks = [];
+
+		this.start();
+	}
+
+	start() {
+		this.raf();
+	}
+
+	stop() {
+		cancelAnimationFrame(this.rafId);
+	}
+
+	raf() {
+		this.callbacks.forEach(({ callback, id }) => callback({ id }));
+		this.rafId = requestAnimationFrame(this.raf);
+	}
+
+	add(callback, id) {
+		this.callbacks.push({ callback, id: id || genId() });
+	}
+
+	remove(id) {
+		this.callbacks = this.callbacks.filter((callback) => callback.id !== id);
+	}
+}
+
+class Vec2 {
+	constructor(x = 0, y = 0) {
+		this.x = x;
+		this.y = y;
+	}
+
+	set(x, y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	lerp(v, t) {
+		this.x = lerp(this.x, v.x, t);
+		this.y = lerp(this.y, v.y, t);
+	}
+}
+
+const vec2 = (x = 0, y = 0) => new Vec2(x, y);
+
+export function tilt(node, options) {
+	let { trigger, target } = resolveOptions(node, options);
+
+	let lerpAmount = 0.06;
+
+	const rotDeg = { current: vec2(), target: vec2() };
+	const bgPos = { current: vec2(), target: vec2() };
+
+	const update = (newOptions) => {
+		destroy();
+		({ trigger, target } = resolveOptions(node, newOptions));
+		init();
+	};
+
+	let rafId;
+
+	function ticker({ id }) {
+		rafId = id;
+
+		rotDeg.current.lerp(rotDeg.target, lerpAmount);
+		bgPos.current.lerp(bgPos.target, lerpAmount);
+
+		for (const el of target) {
+			el.style.setProperty("--rotX", rotDeg.current.y.toFixed(2) + "deg");
+			el.style.setProperty("--rotY", rotDeg.current.x.toFixed(2) + "deg");
+
+			el.style.setProperty("--bgPosX", bgPos.current.x.toFixed(2) + "%");
+			el.style.setProperty("--bgPosY", bgPos.current.y.toFixed(2) + "%");
+		}
+	}
+
+	const onMouseMove = ({ offsetX, offsetY }) => {
+		lerpAmount = 0.1;
+
+		for (const el of target) {
+			const ox = (offsetX - el.clientWidth * 0.5) / (Math.PI * 3);
+			const oy = -(offsetY - el.clientHeight * 0.5) / (Math.PI * 4);
+
+			rotDeg.target.set(ox, oy);
+			bgPos.target.set(-ox * 0.3, oy * 0.3);
+		}
+	};
+
+	const onMouseLeave = () => {
+		lerpAmount = 0.06;
+
+		rotDeg.target.set(0, 0);
+		bgPos.target.set(0, 0);
+	};
+
+	const addListeners = () => {
+		trigger.addEventListener("mousemove", onMouseMove);
+		trigger.addEventListener("mouseleave", onMouseLeave);
+	};
+
+	const removeListeners = () => {
+		trigger.removeEventListener("mousemove", onMouseMove);
+		trigger.removeEventListener("mouseleave", onMouseLeave);
+	};
+
+	const init = () => {
+		addListeners();
+		raf.add(ticker);
+	};
+
+	const destroy = () => {
+		removeListeners();
+		raf.remove(rafId);
+	};
+
+	init();
+
+	return { destroy, update };
+}
+
+function resolveOptions(node, options) {
 	return {
-		x: x * rx,
-		y: y * ry
-	}
-})
-
-const sections = [...document.querySelectorAll('section')]
-const fish = document.querySelector('.fish')
-const fishHeadAndBody = 
-			[
-				...document.querySelectorAll('.fish__head'),
-				...document.querySelectorAll('.fish__body')
-			]
-const lights = [...document.querySelectorAll('[data-lights]')]
-const rays = document.querySelector('[data-rays]')
-
-const bubbles = gsap.timeline()
-bubbles.set('.bubbles__bubble', {
-	y: 100,
-})
-bubbles.to('.bubbles__bubble', {
-	scale: 1.2,
-	y: -300,
-	opacity: 1,
-	duration: 2,
-	stagger: 0.2,
-})
-bubbles.to('.bubbles__bubble', {
-	scale: 1,
-	opacity: 0,
-	duration: 1,
-}, '-=1')
-
-bubbles.pause()
-
-const tl = gsap.timeline({
-	scrollTrigger: {
-		scrub: 1.5,
-	},
-})
-tl.to(fish, {
-	motionPath: {
-		path: scaledPath,
-		align: 'self',
-		alignOrigin: [0.5, 0.5],
-		autoRotate: true
-	},
-	duration: 10,
-	immediateRender: true,
-	// ease: 'power4'
-})
-tl.to('.indicator', {
-	opacity: 0
-}, 0)
-tl.to(fish, {
-	rotateX: 180
-}, 1)
-tl.to(fish, {
-	rotateX: 0
-}, 2.5)
-tl.to(fish, {
-	z: -500,
-	duration: 2,
-}, 2.5)
-tl.to(fish, {
-	rotateX: 180
-}, 4)
-tl.to(fish, {
-	rotateX: 0
-}, 5.5)
-tl.to(fish, {
-	z: -50,
-	duration: 2,
-}, 5)
-tl.to(fish, {
-	rotate: 0,
-	duration: 1,
-}, '-=1')
-tl.to('.fish__skeleton', {
-	opacity: 0.6,
-	duration: 0.1,
-	repeat: 4
-}, '-=3')
-tl.to(fishHeadAndBody, {
-	opacity: 0,
-	duration: 0.1,
-	repeat: 4
-}, '-=3')
-tl.to('.fish__inner', {
-	opacity: 0.1,
-	duration: 1
-}, '-=1')
-tl.to('.fish__skeleton', {
-	opacity: 0.1,
-	duration: 1
-}, '-=1')
-
-bubbles.play()
-tl.pause()
-
-const lightsTl = gsap.timeline({
-	scrollTrigger: {
-		scrub: 6
-	}
-})
-lightsTl.from(lights[0], {
-	x: window.innerWidth * -1,
-	y: window.innerHeight,
-	ease: 'power4.out',
-	duration: 80
-}, 0)
-lightsTl.to(lights[0], {
-	x: window.innerWidth,
-	y: window.innerHeight * -1,
-	ease: 'power4.out',
-	duration: 80
-}, '-=5')
-
-const makeBubbles = (p, i) => {
-	const { top, left } = fish.getBoundingClientRect()
-	gsap.to(p, { opacity: 1, duration: 1 })
-	gsap.set('.bubbles', {
-		x: left,
-		y: top
-	})
-	if (bubbles.paused) {
-		bubbles.restart()
-	}
-	if (i > 6) {
-		gsap.to('.bubbles', {
-			opacity: 0
-		})
-	}
+		trigger: options?.trigger ?? node,
+		target: options?.target
+			? Array.isArray(options.target)
+				? options.target
+				: [options.target]
+			: [node]
+	};
 }
 
-const rotateFish = (self) => {
-	if (self.direction === -1) {
-		gsap.to(fish, { rotationY: 180, duration: 0.4 })
-	} else {
-		gsap.to(fish, { rotationY: 0, duration: 0.4 })
-	}
+// -----------------------------------------------------
+
+// Global Raf Instance
+const raf = new Raf();
+
+function init() {
+	const loader = document.querySelector(".loader");
+
+	const slides = [...document.querySelectorAll(".slide")];
+	const slidesInfo = [...document.querySelectorAll(".slide-info")];
+
+	const buttons = {
+		prev: document.querySelector(".slider--btn__prev"),
+		next: document.querySelector(".slider--btn__next")
+	};
+
+	loader.style.opacity = 0;
+	loader.style.pointerEvents = "none";
+
+	slides.forEach((slide, i) => {
+		const slideInner = slide.querySelector(".slide__inner");
+		const slideInfoInner = slidesInfo[i].querySelector(".slide-info__inner");
+
+		tilt(slide, { target: [slideInner, slideInfoInner] });
+	});
+
+	buttons.prev.addEventListener("click", change(-1));
+	buttons.next.addEventListener("click", change(1));
 }
 
-const hideText = (p) => {
-	gsap.to(p, { opacity: 0, duration: 1 })
-}
+function setup() {
+	const loaderText = document.querySelector(".loader__text");
 
-sections.forEach((section, i) => {
-	const p = section.querySelector('p')
-	gsap.to(p, { opacity: 0 })
-	
-	ScrollTrigger.create({
-		trigger: section,
-		start: "top top",
-		onEnter: () => makeBubbles(p, i),
-		onEnterBack: () => {
-			if (i <= 6) {
-				gsap.to('.bubbles', {
-					opacity: 1
-				})
+	const images = [...document.querySelectorAll("img")];
+	const totalImages = images.length;
+	let loadedImages = 0;
+	let progress = {
+		current: 0,
+		target: 0
+	};
+
+	// update progress target
+	images.forEach((image) => {
+		imagesLoaded(image, (instance) => {
+			if (instance.isComplete) {
+				loadedImages++;
+				progress.target = loadedImages / totalImages;
 			}
-		},
-		onLeave: () => {
-			hideText(p)
-			if (i == 0) {
-			gsap.to('.rays', {
-				opacity: 0,
-				y: -500,
-				duration: 8,
-				ease: 'power4.in'
-			})
-			}
-		},
-		onLeaveBack: () => hideText(p),
-		onUpdate: (self) => rotateFish(self)
-	})
-})
+		});
+	});
+
+	// lerp progress current to progress target
+	raf.add(({ id }) => {
+		progress.current = lerp(progress.current, progress.target, 0.06);
+
+		const progressPercent = Math.round(progress.current * 100);
+		loaderText.textContent = `${progressPercent}%`;
+
+		// hide loader when progress is 100%
+		if (progressPercent === 100) {
+			init();
+
+			// remove raf callback when progress is 100%
+			raf.remove(id);
+		}
+	});
+}
+
+function change(direction) {
+	return () => {
+		let current = {
+			slide: document.querySelector(".slide[data-current]"),
+			slideInfo: document.querySelector(".slide-info[data-current]"),
+			slideBg: document.querySelector(".slide__bg[data-current]")
+		};
+		let previous = {
+			slide: document.querySelector(".slide[data-previous]"),
+			slideInfo: document.querySelector(".slide-info[data-previous]"),
+			slideBg: document.querySelector(".slide__bg[data-previous]")
+		};
+		let next = {
+			slide: document.querySelector(".slide[data-next]"),
+			slideInfo: document.querySelector(".slide-info[data-next]"),
+			slideBg: document.querySelector(".slide__bg[data-next]")
+		};
+
+		Object.values(current).map((el) => el.removeAttribute("data-current"));
+		Object.values(previous).map((el) => el.removeAttribute("data-previous"));
+		Object.values(next).map((el) => el.removeAttribute("data-next"));
+
+		if (direction === 1) {
+			let temp = current;
+			current = next;
+			next = previous;
+			previous = temp;
+
+			current.slide.style.zIndex = "20";
+			previous.slide.style.zIndex = "30";
+			next.slide.style.zIndex = "10";
+		} else if (direction === -1) {
+			let temp = current;
+			current = previous;
+			previous = next;
+			next = temp;
+
+			current.slide.style.zIndex = "20";
+			previous.slide.style.zIndex = "10";
+			next.slide.style.zIndex = "30";
+		}
+
+		Object.values(current).map((el) => el.setAttribute("data-current", ""));
+		Object.values(previous).map((el) => el.setAttribute("data-previous", ""));
+		Object.values(next).map((el) => el.setAttribute("data-next", ""));
+	};
+}
+
+// Start
+setup();
